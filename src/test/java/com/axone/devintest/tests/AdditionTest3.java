28f401c4d86f938c0440af1118b03d6fafc82250
RanjithKumarPutta <ranjithkumarputta@gmail.com>
2024-06-20 21:46:51 +0100

    Added a new test case part of feature1234

---
+++ src/test/java/com/axone/devintest/tests/AdditionTest3.java
@@ -0,0 +1,15 @@
+package com.axone.devintest.tests;
+
+import com.axone.devintest.calc.Addition;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AdditionTest3 {
+
+    @Test
+    public void add(){
+        Addition a = new Addition();
+        int expectedValue = a.addition(30,40);
+        Assert.assertEquals(70,expectedValue);
+    }
+}
